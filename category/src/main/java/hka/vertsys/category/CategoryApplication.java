9ba1fc9f107a97ec48cb90bdc60bf789407bf1c2
Leon Schmidt <leon.schmidt@cas.de>
2024-03-26 09:14:29 +0100

    >Fixed App Startup

--- category/src/main/java/hka/vertsys/category/CategoryApplication.java
+++ category/src/main/java/hka/vertsys/category/CategoryApplication.java
@@ -2,8 +2,9 @@ package hka.vertsys.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CategoryApplication {
 
 	public static void main(String[] args) {
